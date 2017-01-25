04ae94260f532e29b67e457a6cb38d5b8662eb5e
XieQirong <cheerx1994@163.com>
2017-01-25 18:01:11 +0800

    refactored QSLogger
    Signed-off-by: XieQirong <cheerx1994@163.com>

--- src/main/scala/com/qingstor/sdk/utils/QSLogger.scala
+++ src/main/scala/com/qingstor/sdk/utils/QSLogger.scala
@@ -5,17 +5,42 @@ import java.util.logging.{Logger, Level}
 import com.qingstor.sdk.constants.QSConstants
 
 object QSLogger {
-  def setLoggerHandler(loggerName: String): Logger = {
-    val logger = Logger.getLogger(loggerName)
+  private val logger = Logger.getLogger(this.getClass.getName)
+  private var level = "warn"
+  logger.setLevel(Level.WARNING)
 
-    QSConstants.LoggerLevel match {
-      case QSConstants.LoggerFatal => logger.setLevel(Level.SEVERE)
-      case QSConstants.LoggerError => logger.setLevel(Level.SEVERE)
-      case QSConstants.LoggerWarn => logger.setLevel(Level.WARNING)
-      case QSConstants.LoggerDebug => logger.setLevel(Level.ALL)
-      case QSConstants.LoggerInfo => logger.setLevel(Level.INFO)
-      case _ => logger.setLevel(Level.WARNING)
+  def getLevel =  this.level
+
+  def setLevel(level: String) = {
+    level match {
+      case "debug" => logger.setLevel(Level.ALL)
+      case "info" => logger.setLevel(Level.INFO)
+      case "warn" => logger.setLevel(Level.WARNING)
+      case "error" => logger.setLevel(Level.SEVERE)
+      case "fatal" => logger.setLevel(Level.SEVERE)
+      case _ => fatal("Log level invalid: " + level)
     }
-    logger
+    this.level = level
+  }
+
+  def debug(msg: String) = {
+    logger.log(Level.ALL, msg)
+  }
+
+  def info(msg: String) = {
+    logger.log(Level.INFO, msg)
+  }
+
+  def warn(msg: String) = {
+    logger.log(Level.WARNING, msg)
+  }
+
+  def error(msg: String) = {
+    logger.log(Level.SEVERE, msg)
+  }
+
+  def fatal(msg: String) = {
+    logger.log(Level.SEVERE, msg)
+    scala.sys.exit(-1)
   }
 }
