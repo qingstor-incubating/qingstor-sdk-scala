034eb7242cf2d29ac768b41f6b91850a22710767
XieQirong <cheerx1994@163.com>
2017-02-28 23:40:58 +0800

    add getHmacSHA256
    Signed-off-by: XieQirong <cheerx1994@163.com>

--- src/main/scala/com/qingstor/sdk/util/SecurityUtil.scala
+++ src/main/scala/com/qingstor/sdk/util/SecurityUtil.scala
@@ -2,10 +2,19 @@ package com.qingstor.sdk.util
 
 import java.security.MessageDigest
 import java.util.Base64
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 object SecurityUtil {
   def getMD5(string: String): Array[Byte] = {
     MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"))
+  }
+
+  def getHmacSHA256(key: String, content: String): Array[Byte] = {
+    val secret = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256")
+    val mac = Mac.getInstance("HmacSHA256")
+    mac.init(secret)
+    mac.doFinal(content.getBytes("UTF-8"))
   }
 
   def encodeToBase64String(bytes: Array[Byte]): String = {
