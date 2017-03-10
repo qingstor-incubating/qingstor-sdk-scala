c200157ea1a35800f1a162393326ba51754cb773
XieQirong <cheerx1994@163.com>
2017-03-10 19:33:44 +0800

    update fileds of QSHttpResponse
    Signed-off-by: XieQirong <cheerx1994@163.com>

--- src/main/scala/com/qingstor/sdk/model/QSModels.scala
+++ src/main/scala/com/qingstor/sdk/model/QSModels.scala
@@ -20,6 +20,14 @@ object QSModels {
     private var server: String = _
     private var contentRange: String = _
     private var xQSEncryptionCustomerAlgorithm: String = _
+    private var contentLength: String = _
+    private var contentType: String = _
+    private var accessControlAllowHeaders: String = _
+    private var accessControlAllowMethods: String = _
+    private var accessControlAllowOrigin: String = _
+    private var accessControlExposeHeaders: String = _
+    private var accessControlMaxAge: String = _
+    private var lastModified: String = _
 
     def setStatusCode(statusCode: Int): Unit = this.statusCode = statusCode
     def setRequestID(requestID: String): Unit = this.requestID = requestID
@@ -30,6 +38,14 @@ object QSModels {
     def setServer(server: String): Unit = this.server = server
     def setContentRange(range: String): Unit = this.contentRange = range
     def setXQSEncryptionCustomerAlgorithm(algorithm: String): Unit = this.xQSEncryptionCustomerAlgorithm = algorithm
+    def setContentLength(length: String): Unit = this.contentLength = length
+    def setContentType(t: String): Unit = this.contentType = t
+    def setAccessControlAllowHeaders(headers: String): Unit = this.accessControlAllowHeaders = headers
+    def setAccessControlAllowMethods(methods: String): Unit = this.accessControlAllowMethods = methods
+    def setAccessControlAllowOrigin(str: String): Unit = this.accessControlAllowOrigin = str
+    def setAccessControlExposeHeaders(str: String): Unit = this.accessControlExposeHeaders = str
+    def setAccessControlMaxAge(str: String): Unit = this.accessControlMaxAge = str
+    def setLastModified(str: String): Unit = this.lastModified = str
 
     @ParamAnnotation(location = "StatusCode")
     def getStatusCode: Int = this.statusCode
@@ -57,6 +73,30 @@ object QSModels {
 
     @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "X-QS-Encryption-Customer-Algorithm")
     def getXQSEncryptionCustomerAlgorithm: String = this.xQSEncryptionCustomerAlgorithm
+
+    @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "Content-Length")
+    def getContentLength: String = this.contentLength
+
+    @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "Content-Type")
+    def getContentType: String = this.contentType
+
+    @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "Access-Control-Allow-Headers")
+    def getAccessControlAllowHeaders: String = this.accessControlAllowHeaders
+
+    @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "Access-Control-Allow-Methods")
+    def getAccessControlAllowMethods: String = this.accessControlAllowMethods
+
+    @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "Access-Control-Allow-Origin")
+    def getAccessControlAllowOrigin: String = this.accessControlAllowOrigin
+
+    @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "Access-Control-Expose-Headers")
+    def getAccessControlExposeHeaders: String = this.accessControlExposeHeaders
+
+    @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "Access-Control-Max-Age")
+    def getAccessControlMaxAge: String = this.accessControlMaxAge
+
+    @ParamAnnotation(location = QSConstants.ParamsLocationHeader, name = "Last-Modified")
+    def getLastModified: String = this.lastModified
   }
 
   case class ErrorMessage(
