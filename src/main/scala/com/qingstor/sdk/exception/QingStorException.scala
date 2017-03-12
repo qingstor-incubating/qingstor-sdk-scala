373e0b6c241ac3d07a49a5ba2a4e87ae89623a6b
XieQirong <cheerx1994@163.com>
2017-03-12 16:46:49 +0800

    use ErrorMessage to construct QingStorException, it will only be used while error in request
    Signed-off-by: XieQirong <cheerx1994@163.com>

--- src/main/scala/com/qingstor/sdk/exception/QingStorException.scala
+++ src/main/scala/com/qingstor/sdk/exception/QingStorException.scala
@@ -2,43 +2,39 @@ package com.qingstor.sdk.exception
 
 import com.qingstor.sdk.model.QSModels.ErrorMessage
 
-class QingStorException(msg: String) extends Throwable {
-  private val message = msg
+class QingStorException(error: ErrorMessage) extends Throwable {
+  def errorMessage: ErrorMessage = error
 
   override def getCause: Throwable = super.getCause
 
   override def printStackTrace(): Unit = super.printStackTrace()
 
-  override def getMessage: String = message
-
-}
-
-object QingStorException {
-  def apply(error: ErrorMessage): QingStorException = {
-    val errorMessage = "request_id " + error.requestID + (
-        error.statusCode match {
-        case Some(code) => ", status_code " + String.valueOf(code)
+  override def getMessage: String = {
+    val errorMessage = "request_id: " + error.requestID + (
+      error.statusCode match {
+        case Some(code) => ", status_code: " + String.valueOf(code)
         case None => ""
       }
       ) + (
-        error.code match {
-        case Some(code) => ", code " + code
+      error.code match {
+        case Some(code) => ", code: " + code
         case None => ""
       }
       ) + (
-        error.message match {
-        case Some(message) => ", message " + message
+      error.message match {
+        case Some(message) => ", message: " + message
         case None => ""
       }
       ) + (
-        error.url match {
-        case Some(url) => ", url " + url
+      error.url match {
+        case Some(url) => ", url: " + url
         case None => ""
       }
       )
-    val msg = "QingStor exception: %s".format(errorMessage)
-    new QingStorException(msg)
+    "QingStor exception:\n\t%s".format(errorMessage)
   }
+}
 
-  def apply(msg: String): QingStorException = new QingStorException(msg)
+object QingStorException {
+  def apply(error: ErrorMessage): QingStorException = new QingStorException(error)
 }
