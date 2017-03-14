c44c47734742a89a48be4f253d797e6f2f9be87e
XieQirong <cheerx1994@163.com>
2017-03-14 13:30:12 +0800

    test for service.feature done
    Signed-off-by: XieQirong <cheerx1994@163.com>

--- test/steps/QingStorSteps.scala
+++ test/steps/QingStorSteps.scala
@@ -7,45 +7,41 @@ import cucumber.api.java8.En
 import com.qingstor.sdk.service.QingStor
 import com.qingstor.sdk.service.QingStor._
 import TestUtil.TestConfig
-import cucumber.runtime.java.picocontainer.PicoFactory
 
 import scala.concurrent.{Await, ExecutionContextExecutor}
 import scala.concurrent.duration.Duration
 
-class QingStorTest(dep: QingStorDependency) extends En {
+class QingStorSteps extends En {
   implicit val system = ActorSystem()
   implicit val mat = ActorMaterializer()
   implicit val ec: ExecutionContextExecutor = system.dispatcher
-  private val qingStorDependency = dep
 
-//  private var config: QSConfig = _
-//  private var testConfig: TestConfig = _
-//  private var qingStor: QingStor = _
-//  private var listBucketsOutput: ListBucketsOutput = _
-
-  Given("^need to use QingStor service$", () => {
-    qingStorDependency.config = TestUtil.getQSConfig
-    qingStorDependency.testConfig = TestUtil.getTestConfig
-    assert(qingStorDependency.config != null)
-    assert(qingStorDependency.testConfig != null)
-  })
+  private var config: QSConfig = _
+  private var testConfig: TestConfig = _
+  private var qingStor: QingStor = _
+  private var listBucketsOutput: ListBucketsOutput = _
 
   When("^initialize QingStor service$", () => {
-    qingStorDependency.qingStor = QingStor(qingStorDependency.config)
+    config = TestUtil.getQSConfig
+    testConfig = TestUtil.getTestConfig
+    qingStor = QingStor(config)
   })
 
   Then("^the QingStor service is initialized$", () => {
-    assert(qingStorDependency.qingStor != null)
+    if (qingStor == null)
+      throw new NullPointerException("QingStor service is not initialized")
   })
 
   When("^list buckets$", () => {
-    val input = ListBucketsInput(Option(qingStorDependency.testConfig.zone))
-    qingStorDependency.listBucketsOutput = Await.result(qingStorDependency.qingStor.listBuckets(input), Duration.Inf)
-    assert(qingStorDependency.listBucketsOutput != null)
+    config = TestUtil.getQSConfig
+    testConfig = TestUtil.getTestConfig
+    qingStor = QingStor(config)
+    val input = ListBucketsInput(Option(testConfig.zone))
+    listBucketsOutput = Await.result(qingStor.listBuckets(input), Duration.Inf)
   })
 
-  Then("^list buckets status code is (\\d+)$", (status: Int) => {
-    assert(qingStorDependency.listBucketsOutput.statusCode.getOrElse(0) == status)
+  Then("^list buckets status code is (\\d+)$", (status: Integer) => {
+    assert(int2Integer(listBucketsOutput.statusCode.getOrElse(0)) == status)
   })
 
 }
