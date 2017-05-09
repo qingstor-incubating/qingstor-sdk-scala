9d755a6a48dbbf7b80cabdcc58c23a6a9c35ca45
XieQirong <cheerx1994@163.com>
2017-05-09 17:26:23 +0800

    Fixed indent error
    Signed-off-by: XieQirong <cheerx1994@163.com>

--- src/main/scala/com/qingstor/sdk/exception/QingStorException.scala
+++ src/main/scala/com/qingstor/sdk/exception/QingStorException.scala
@@ -12,25 +12,25 @@ class QingStorException(error: ErrorMessage) extends Throwable {
 
   override def getMessage: String = {
     val errorMessage = "request_id: " + error.requestID + (
-      error.statusCode match {
-        case Some(code) => ", status_code: " + String.valueOf(code)
-        case None => ""
-      }
+        error.statusCode match {
+          case Some(code) => ", status_code: " + String.valueOf(code)
+          case None => ""
+        }
       ) + (
-      error.code match {
-        case Some(code) => ", code: " + code
-        case None => ""
-      }
+        error.code match {
+          case Some(code) => ", code: " + code
+          case None => ""
+        }
       ) + (
-      error.message match {
-        case Some(message) => ", message: " + message
-        case None => ""
-      }
+        error.message match {
+          case Some(message) => ", message: " + message
+          case None => ""
+        }
       ) + (
-      error.url match {
-        case Some(url) => ", url: " + url
-        case None => ""
-      }
+        error.url match {
+          case Some(url) => ", url: " + url
+          case None => ""
+        }
       )
     "QingStor exception:\n\t%s".format(errorMessage)
   }
